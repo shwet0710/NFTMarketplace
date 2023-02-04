10772bcd036bde31b5070e8d77db831b6ba98d71
shwet0710 <shwetshukla0710@gmail.com>
2023-02-04 18:34:52 +0530

    added more frontend part

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -1,9 +1,28 @@
 import Button from "./Button/Button";
 import Footer from "./Footer/Footer";
 import NavBar from "./NavBar/NavBar";
+import HeroSection from "./HeroSection/HeroSection";
+import Service from "./Service/Service";
+import BigNFTSilder from "./BigNFTSilder/BigNFTSilder";
+import Subscribe from "./Subscribe/Subscribe";
+import Title from "./Title/Title";
+import Category from "./Category/Category";
+import Filter from "./Filter/Filter";
+import NFTCard from "./NFTCard/NFTCard";
+import Collection from "./Collection/Collection";
+
 
 export {
     Button,
     Footer,
     NavBar,
+    HeroSection,
+    Service,
+    BigNFTSilder,
+    Subscribe,
+    Title,
+    Category,
+    Filter,
+    NFTCard,
+    Collection
 };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
 import React from "react";
 
+import Style from "../styles/index.module.css";
+import { HeroSection, Service, BigNFTSilder, Subscribe, Title, Category, Filter, NFTCard, Collection } from "../components/componentsindex";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+      <Service />
+      <BigNFTSilder />
+      <Title
+        heading="New Collection"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <Collection />
+      <Title
+        heading="Featured NFTs"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <Filter />
+      <NFTCard />
+      <Title
+        heading="Browse by category"
+        paragraph="Explore the NFTs in the most featured categories."
+      />
+      <Category />
+      <Subscribe />
+    </div>
+  );
 }
 
 export default Home;

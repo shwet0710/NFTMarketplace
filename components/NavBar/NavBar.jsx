78ae8637bbe092c813c4d5961d30d31c182d5334
Ritik Raj <rajatraj9559@gmail.com>
2023-02-06 16:13:47 +0530

    application icon designed and updated

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -78,8 +78,8 @@ const NavBar = () => {
             <Image
               src={images.logo}
               alt="NFT MARKET PLACE"
-              width={100}
-              height={100}
+              width={50}
+              height={50}
             />
           </div>
           <div className={Style.navbar_container_left_box_input}>
